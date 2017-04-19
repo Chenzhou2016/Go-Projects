2b0e364d81caba3ed8a56722b06119b32bc00ffa
Chenzhou2016 <zhou.chen@hpe.com>
2017-04-19 08:45:06 +0000

    First library

--- src/helloworld/hello.go
+++ src/helloworld/hello.go
@@ -3,9 +3,12 @@ package main
 import (
 	"fmt"
 	"strings"
+	"user/stringutil"
 )
 
 func main() {
 	fmt.Println("Hello, Go!")
 	fmt.Println(strings.ToUpper("This upper case string"))
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+
 }
